835cc29500f27799be04367e7b4b19efa191f40e
Maxim Vitoshka-Tarasov <maxeeem@gmail.com>
2014-11-17 21:52:20 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* SessionPath navigation example
+*
+* @author Maxim Vitoshka-Tarasov
+*/
+
+/*
+* (re)set defaults
+*/
+$_SESSION['default'] = true;
+$_SESSION['name']    = 'World';
+?>
+<html>
+<head>
+<title>SessionPath navigation example</title>
+</head>
+<body>
+<center>
+<h3><a href="./hello">Hello World example</a></h3>
+</center>
+</body>
+</html>
